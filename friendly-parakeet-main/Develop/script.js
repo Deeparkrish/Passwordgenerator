e07edd324c91e1198198a4369ff95f551f706828
Deepa <kosalai17@gmail.com>
2021-05-22 11:10:39 -0700

    added code to get user input

--- friendly-parakeet-main/Develop/script.js
+++ friendly-parakeet-main/Develop/script.js
@@ -1,6 +1,58 @@
 // Assignment code here
+var alphabetArray ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericArray= '0123456789';
+var specialCharArray = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+var passwordEl ="";
+var pwdLength=0;
+var checkUcaseAlphabet=false;
+var checkSpecial =false;
+var checkLcaseAlphabet= false;
+var checkNumeric =false;
+
+var getPasswordLength= function(){
+  pwdLength=window.prompt("Please enter a password length greater than 7 and less than 129");
+  console.log(pwdLength);
+  if (isNaN(pwdLength)) 
+  {
+      alert("Must input numbers");
+      getPasswordLength();
+  }
+  else if (pwdLength=== null || pwdLength<=0 ) {
+    alert("Please enter a valid length");
+    getPasswordLength();
+  }
+  else if (pwdLength < 8 ||  pwdLength >129)
+  {
+    alert(" Password length must be between 8-128 characters");
+    getPasswordLength();
+  }
+  else{
+    length =Math.round(parseInt(length,10));
+  }
+
+  console.log(pwdLength);
+};
+var charactersToInclude =function(){
+
+  checkUcaseAlphabet =window.confirm("Would you like to include upper case alphabets in your password?")
+    checkLcaseAlphabet =window.confirm("Would you like to include lower case alphabets in your password?")
+    checkNumeric =window.confirm("Would you like to include numbers in your password ?");
+    checkSpecial =window.confirm("Would you like to include special characters in your password?")
+    
+    console.log(checkUcaseAlphabet);
+    console.log(checkNumeric);
+    console.log(checkSpecial);
+    console.log(checkLcaseAlphabet);
+};
 
 
+var generatePassword =function(){
+    // get the password length from user
+    getPasswordLength();
+    charactersToInclude();
+    
+  
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -8,7 +60,6 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
